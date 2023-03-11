a1d5daaec5e46e0d85aae91e676d0d398c3de7b1
satheeskaran <satheeskaran@huex.io>
2023-03-11 07:02:58 +0530

    .

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { useSelector } from "react-redux";
 import {
   Container,
@@ -10,23 +9,18 @@ import Profile from "./components/Profile";
 import NewsFeed from "./components/NewsFeed";
 import Advert from "./components/Advert";
 import {
-  useCreatePostMutation,
   useGetPostsQuery,
   useDeletePostMutation,
 } from "../../service/postService";
 
 const Home = () => {
-  const [deletingPostId, setDeletingPostId] = useState(null);
   const user = useSelector((state) => state?.persist?.auth?.user);
 
-  const [createPost] = useCreatePostMutation();
-
-  const { isLoading, data: posts } = useGetPostsQuery();
+  const { data: posts } = useGetPostsQuery();
 
   //delete post query call
 
-  const [deletePost, { isLoading: isPostDeleteLoading }] =
-    useDeletePostMutation();
+  const [deletePost] = useDeletePostMutation();
 
   // const handleNewPostFormSubmit = async (data) => {
   //   console.log("function called");
@@ -41,7 +35,6 @@ const Home = () => {
 
   // Post delete function
   const handleDeletePost = async (id) => {
-    setDeletingPostId(id);
     await deletePost(id);
   };
   return (
